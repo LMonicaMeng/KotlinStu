48577776b3be7d0b3c753ae2259152fa9c09a447
mengxin01-bt <mengxin01@btime.com>
2020-09-11 11:37:20 +0800

    Kotlin 函数

---
+++ restukotlin/src/main/java/com/btime/restukotlin/BuiltinTypes/function/JavaMethods.java
@@ -0,0 +1,10 @@
+package com.btime.restukotlin.BuiltinTypes.function;
+
+import java.util.Arrays;
+
+public class JavaMethods {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
